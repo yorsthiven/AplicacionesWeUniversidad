57968fbf23ddf027823433802a99e3a4492a34c0
stiven medina <69610934+yorsthiven@users.noreply.github.com>
2025-11-27 12:54:04 -0500

    Primer commit del proyecto rituales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { InicioPageComponent } from './pages/inicio/inicioPageComponent';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: InicioPageComponent,
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+];
---
+++ src/app/pages/inicio/inicioPageComponent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inicio',
+  imports: [],
+  templateUrl: './inicioPageComponent.html',
+  styleUrl: './inicioPageComponent.css',
+})
+export class InicioPageComponent {
+
+}
